ee5ef477186cfe4716a7322561e254a92748b61d
DARANCODE <99070344+DARANCODE@users.noreply.github.com>
2022-10-20 18:23:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'For U';
+const giftUrl = 'facebook.com/daran06';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Happy woman s day';
+const contentCard = 'Chúc mừng ngày phụ nữ Việt Nam 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với các bạn nữ trong hôm nay và cả những ngày sau :33 ';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
